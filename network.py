04928ac993eb6da669d14d83bd7ad3c477118c85
yinguobing <yinguobing@gmail.com>
2020-12-02 15:02:04 +0800

    fix decoding layer

--- network.py
+++ network.py
@@ -9,6 +9,7 @@ feature maps at strides 8, 16 and 32.
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+from anchor import convert_to_corners, AnchorBox
 
 from models.shufflenet_v2 import shuffle_unit_v2
 
@@ -173,12 +174,15 @@ def build_retinanet(num_classes, backbone=None):
     return keras.Model(inputs=inputs, outputs=outputs, name="retinanet")
 
 
-# Implementing a custom layer to decode predictions
+def build_decoding_layer(num_classes=2,
+                         confidence_threshold=0.05,
+                         nms_iou_threshold=0.5,
+                         max_detections_per_class=100,
+                         max_detections=100,
+                         box_variance=[0.1, 0.1, 0.2, 0.2],):
+    """Decodes predictions of the RetinaNet model.
 
-class DecodePredictions(tf.keras.layers.Layer):
-    """A Keras layer that decodes predictions of the RetinaNet model.
-
-    Attributes:
+    Args:
         num_classes: Number of classes in the dataset
         confidence_threshold: Minimum class probability, below which detections
             are pruned.
@@ -190,59 +194,38 @@ class DecodePredictions(tf.keras.layers.Layer):
         box_variance: The scaling factors used to scale the bounding box
             predictions.
     """
+    _anchor_box = AnchorBox()
+    _box_variance = tf.convert_to_tensor(
+        [0.1, 0.1, 0.2, 0.2], dtype=tf.float32)
 
-    def __init__(
-        self,
-        num_classes=80,
-        confidence_threshold=0.05,
-        nms_iou_threshold=0.5,
-        max_detections_per_class=100,
-        max_detections=100,
-        box_variance=[0.1, 0.1, 0.2, 0.2],
-        **kwargs
-    ):
-        super(DecodePredictions, self).__init__(**kwargs)
-        self.num_classes = num_classes
-        self.confidence_threshold = confidence_threshold
-        self.nms_iou_threshold = nms_iou_threshold
-        self.max_detections_per_class = max_detections_per_class
-        self.max_detections = max_detections
-
-        self._anchor_box = AnchorBox()
-        self._box_variance = tf.convert_to_tensor(
-            [0.1, 0.1, 0.2, 0.2], dtype=tf.float32
-        )
-
-    def _decode_box_predictions(self, anchor_boxes, box_predictions):
-        boxes = box_predictions * self._box_variance
-        boxes = tf.concat(
-            [
-                boxes[:, :, :2] * anchor_boxes[:, :, 2:] +
-                anchor_boxes[:, :, :2],
-                tf.math.exp(boxes[:, :, 2:]) * anchor_boxes[:, :, 2:],
-            ],
-            axis=-1,
-        )
+    def _decode_box_predictions(anchor_boxes, box_predictions):
+        boxes = box_predictions * _box_variance
+        boxes = tf.concat([boxes[:, :, :2] * anchor_boxes[:, :, 2:] +
+                           anchor_boxes[:, :, :2],
+                           tf.math.exp(boxes[:, :, 2:]) * anchor_boxes[:, :, 2:]],
+                          axis=-1)
         boxes_transformed = convert_to_corners(boxes)
+
         return boxes_transformed
 
-    def call(self, images, predictions):
+    def forward(images, predictions):
         image_shape = tf.cast(tf.shape(images), dtype=tf.float32)
-        anchor_boxes = self._anchor_box.get_anchors(
-            image_shape[1], image_shape[2])
+        anchor_boxes = _anchor_box.get_anchors(image_shape[1], image_shape[2])
         box_predictions = predictions[:, :, :4]
         cls_predictions = tf.nn.sigmoid(predictions[:, :, 4:])
-        boxes = self._decode_box_predictions(
+        boxes = _decode_box_predictions(
             anchor_boxes[None, ...], box_predictions)
 
         return tf.image.combined_non_max_suppression(
             tf.expand_dims(boxes, axis=2),
             cls_predictions,
-            self.max_detections_per_class,
-            self.max_detections,
-            self.nms_iou_threshold,
-            self.confidence_threshold,
+            max_detections_per_class,
+            max_detections,
+            nms_iou_threshold,
+            confidence_threshold,
             clip_boxes=False)
+
+    return forward
 
 
 def build_model(num_classes):
