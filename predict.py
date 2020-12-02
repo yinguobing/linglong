42eec8cc935e32c8b3729c52680ab6fd7ea9eaa6
yinguobing <yinguobing@gmail.com>
2020-12-02 15:02:40 +0800

    support inference with video stream

--- predict.py
+++ predict.py
@@ -1,47 +1,83 @@
+from argparse import ArgumentParser
+
+import cv2
+import numpy as np
 import tensorflow as tf
-from network import build_model
+
+from network import build_decoding_layer, build_model
+from preprocessing import resize_and_pad_image
+
+# Take arguments from user input.
+parser = ArgumentParser()
+parser.add_argument("--video", type=str, default=None,
+                    help="Video file to be processed.")
+parser.add_argument("--cam", type=int, default=None,
+                    help="The webcam index.")
+args = parser.parse_args()
+
 
 if __name__ == "__main__":
 
     # Initializing the model
-    model = build_model()
+    model = build_model(2)
 
-    # Loading weights. Change this to `model_dir` when not using the downloaded
-    # weights.
-    weights_dir = "data"
-
-    latest_checkpoint = tf.train.latest_checkpoint(weights_dir)
+    # Loading weights.
+    latest_checkpoint = tf.train.latest_checkpoint("checkpoints")
     model.load_weights(latest_checkpoint)
 
     # Building inference model
-
     image = tf.keras.Input(shape=[None, None, 3], name="image")
     predictions = model(image, training=False)
-    detections = DecodePredictions(
-        confidence_threshold=0.5)(image, predictions)
+    detections = build_decoding_layer(
+        2, confidence_threshold=0.6)(image, predictions)
     inference_model = tf.keras.Model(inputs=image, outputs=detections)
 
     # Generating detections
-
     def prepare_image(image):
         image, _, ratio = resize_and_pad_image(image, jitter=None)
         image = tf.keras.applications.resnet.preprocess_input(image)
         return tf.expand_dims(image, axis=0), ratio
 
-    val_dataset = tfds.load("coco/2017", split="validation", data_dir="data")
-    int2str = dataset_info.features["objects"]["label"].int2str
+    # Video source from webcam or video file.
+    video_src = args.cam if args.cam is not None else args.video
+    if video_src is None:
+        print("Warning: video source not assigned, default webcam will be used.")
+        video_src = 0
 
-    for sample in val_dataset.take(2):
-        image = tf.cast(sample["image"], dtype=tf.float32)
-        input_image, ratio = prepare_image(image)
+    cap = cv2.VideoCapture(video_src)
+    if video_src == 0:
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+
+    while True:
+        # Read frame, crop it, flip it, suits your needs.
+        frame_got, frame = cap.read()
+        if frame_got is False:
+            break
+
+        # Crop it if frame is larger than expected.
+        # frame = frame[:480, :640]
+
+        # If frame comes from webcam, flip it so it looks like a mirror.
+        if video_src == 0:
+            frame = cv2.flip(frame, 2)
+
+        # Read in and preprocess the sample image
+        input_image, ratio = prepare_image(frame)
+
+        # Do prediction.
         detections = inference_model.predict(input_image)
+
+        # Parse the detection result.
         num_detections = detections.valid_detections[0]
-        class_names = [
-            int2str(int(x)) for x in detections.nmsed_classes[0][:num_detections]
-        ]
-        visualize_detections(
-            image,
-            detections.nmsed_boxes[0][:num_detections] / ratio,
-            class_names,
-            detections.nmsed_scores[0][:num_detections],
-        )
+        boxes = detections.nmsed_boxes[0][:num_detections] / ratio
+        scores = detections.nmsed_scores[0][:num_detections]
+        for box, score in zip(boxes, scores):
+            x1, y1, x2, y2 = np.array(box, dtype=int)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(frame, "face:{:.2}".format(
+                score), (x1, y1-5), cv2.FONT_HERSHEY_DUPLEX, 0.6, (0, 255, 0))
+
+        # Show the result in windows.
+        cv2.imshow('image', frame)
+        if cv2.waitKey(1) == 27:
+            break
